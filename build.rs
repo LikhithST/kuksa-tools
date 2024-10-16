651405ec80b0b6c5a025de08980c0f54f67d80d9
LikhithST <LikhithSadahalliThammegowda@gmail.com>
2024-10-16 08:46:31 +0200

    kuksa-tools project setup

---
+++ build.rs
@@ -0,0 +1,11 @@
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tonic_build::configure()
+        .compile(
+            &["proto/kuksa/val/v1/val.proto", "proto/kuksa/val/v1/types.proto"], // Compile both val.proto and types.proto
+            &["proto"],  // The base directory containing the .proto files
+        )
+        .unwrap_or_else(|e| panic!("Failed to compile protos: {:?}", e));
+
+    println!("OUT_DIR = {:?}", std::env::var("OUT_DIR"));
+    Ok(())
+}
